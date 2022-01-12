2bafd15624369b8a50de08464cad40b64fd231ee
Adamfomin-skc <96421014+Adamfomin-skc@users.noreply.github.com>
2022-01-12 10:29:22 +0000

    Add files via upload

---
+++ week 2 coding/Escape sequences 130921.py
@@ -0,0 +1,6 @@
+#print("dylan said \"hello world\"")
+#we use \ key to introduce an escape sequences, in other words it tells python to do something
+#when it sees "
+#print("hello \n world")
+#no space after the n will ensure tha the w in world starts in right position otherwise it will show a space
+print ("hello \n\world")
---
+++ week 2 coding/random library 170921.py
@@ -0,0 +1,7 @@
+from random import *
+number = str(chance('heads','tails'))
+print (number)
+
+#import random
+#number = random.randint(1,2)
+#print (number)
